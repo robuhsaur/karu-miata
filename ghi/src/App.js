59bff5fd947a32967a2eea519741ba2c24057f44
Robert Nacario <r.nacariojr24@gmail.com>
2023-02-05 23:15:45 -0800

    created react app

---
+++ ghi/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1>
+            This will be the landing page
+          </h1>
+        </p>
+        <a>
+          Look at your stats
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
